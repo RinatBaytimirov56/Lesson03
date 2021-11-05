84a7ce77f2e1a56659716e7e1610dc444ec0a32e
ринат <tatarin03101995@gmail.com>
2021-11-05 17:05:33 +0500

    sffs

---
+++ task002/Program.cs
@@ -0,0 +1,6 @@
+﻿//По двум заданным числам проверять является ли первое квадратом 
+int a,b;
+a=3;b=7;
+Console.WriteLine(Math.Pow(a,2));
+Console.WriteLine(b);
+Console.ReadKey();
---
+++ task003/Program.cs
@@ -0,0 +1,7 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+int min, max;
+min = 3; max = 43;
+Console.WriteLine(min);
+Console.WriteLine(max);
+Console.ReadKey();
