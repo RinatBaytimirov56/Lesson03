b3f36f535273da8d5811bd5d7996c2d17a8dea4e
ринат <tatarin03101995@gmail.com>
2021-11-11 00:51:44 +0500

    с 9 по 12 и 27

---
+++ task009/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.WriteLine("Ведите трехзначное число ");
+int number;
+number= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Последняя цифра");
+Console.WriteLine(number%10);
