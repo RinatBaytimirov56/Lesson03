c07832af900cec35a1fd0064b26f9744f09bd44a
ринат <tatarin03101995@gmail.com>
2021-11-11 00:52:42 +0500

    с 9 по 12 и 27

---
+++ task010/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.WriteLine("Ведите трехзначное число ");
+int number;
+number= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вторая цифра");
+Console.WriteLine(number%100/10);
