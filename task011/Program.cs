26146a0d252a042f8e2304a3193e7863aac76fde
ринат <tatarin03101995@gmail.com>
2021-11-11 00:53:13 +0500

    с 9 по 12 и 27

---
+++ task011/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int numbernumber = new Random().Next(10,99);
+Console.WriteLine("Рандомное число с отрезка");
+Console.WriteLine(numbernumber);
+Console.WriteLine("наибольшая цифра числа");
+int secondnumber = numbernumber%10;
+int firstnumber = (numbernumber - secondnumber)/10;
+if (firstnumber > secondnumber)
+{
+    Console.WriteLine(firstnumber);
+}
+else Console.WriteLine(secondnumber);
