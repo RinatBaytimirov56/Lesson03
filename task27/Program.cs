4ab4f4e2f0f2a8b863c381d00622e526d653fb37
ринат <tatarin03101995@gmail.com>
2021-11-11 00:54:34 +0500

    Все списал))

---
+++ task27/Program.cs
@@ -0,0 +1,15 @@
+﻿// 27. Определить кол-во цифр в числе
+//4567 - 4
+
+int CountDigit(int n)
+{
+    int k=0;
+    while(n!=0)
+    {
+        k++;
+        n=n/10;
+    }
+    return k;
+}
+
+Console.WriteLine(CountDigit(1234969));
